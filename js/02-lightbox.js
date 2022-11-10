29e6281e967d54de54e60196c46d3e3e4e36c43c
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-11-10 20:23:13 +0700

    copied code

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click',
+    onGalleryContainerClick);
+
+galleryContainer.insertAdjacentHTML('beforeend',
+    galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" />
+            </a>
+            `;
+        }
+    ).join('');
+};
+
+function onGalleryContainerClick(event) {
+    blockDefaultAction(event);
+    
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    };
+};
+
+function blockDefaultAction(event) {
+    event.preventDefault();
+};
